18c9c864ebbd5d0b1c9786b01b9dc428633606b6
okue_hidetaka <okue@as-systems.jp>
2025-10-16 22:59:54 +0900

    admob広告のidの登録とinfo.plistの変更

--- SimpleWorkoutMemo/EnvironmentConstant.swift
+++ SimpleWorkoutMemo/EnvironmentConstant.swift
@@ -12,4 +12,14 @@ class EnvironmentConstant {
     
     static let privacyPolicyURL = URL(string: "https://simple-workout-privacy.web.app")!
     static let termOfServiceURL =  URL(string: "https://simple-workout-terms.web.app")!
+    
+    #if DEBUG
+    static let bannerId = "ca-app-pub-3940256099942544/2435281174"
+    static let interstitialId = "ca-app-pub-3940256099942544/4411468910"
+    static let adOpenId = "ca-app-pub-3940256099942544/5575463023"
+    #elseif PRODUCTION
+    static let bannerId = "ca-app-pub-6663259427797114/7797007267"
+    static let interstitialId = "ca-app-pub-6663259427797114/4280240568"
+    static let adOpenId = "ca-app-pub-6663259427797114/3154667637"
+    #endif
 }
