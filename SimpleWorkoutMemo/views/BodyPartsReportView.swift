6d688ea518182242809158d9ead7350f5b763485
okue_hidetaka <okue@as-systems.jp>
2025-10-26 23:46:57 +0900

    レポート画面のUI修正

--- SimpleWorkoutMemo/views/BodyPartsReportView.swift
+++ SimpleWorkoutMemo/views/BodyPartsReportView.swift
@@ -16,24 +16,35 @@ struct BodyPartsReportView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Picker("Segment", selection: $timePeriod) {
-                    ForEach(TimePeriod.nonAllCases, id: \.id) { period in
-                        Text(period.title)
-                            .tag(period)
+            VStack(spacing: 0) {
+                VStack(spacing: 6) {
+                    Picker("Segment", selection: $timePeriod) {
+                        ForEach(TimePeriod.nonAllCases, id: \.id) { period in
+                            Text(period.title)
+                                .tag(period)
+                        }
+                    }.pickerStyle(.segmented)
+                    HStack {
+                        Text("※部位毎のそれぞれの種目のトータルを過去と比較しているグラフです。")
+                            .foregroundStyle(.gray)
+                            .font(.regular(size: 12))
+                        Spacer()
                     }
-                }.pickerStyle(.segmented)
-                    .padding(.horizontal, 20)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 20)
                 BannerViewContainer {
                     isHideBanner = true
                 }
                 .frame(maxWidth: .infinity)
                 .frame(height: isHideBanner ? 0 : 50)
+                .padding(.bottom, isHideBanner ? 0 : 20)
                 ForEach(Parts.allCases, id: \.id) { parts in
                     BodyPartsReportItemView(timePeriod: $timePeriod,
                                             viewModel: viewModel,
                                             parts: parts)
                 }
+                .padding(.bottom, 20)
             }
         }
         .navigationTitle("部位別レポート")
