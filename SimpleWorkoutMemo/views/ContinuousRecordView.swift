be0bad8ea6a527232c02345ad3667cba0ed2f0d2
okue_hidetaka <okue@as-systems.jp>
2025-10-29 17:55:42 +0900

    今週目標達成時にレポート画面で紙吹雪アニメーションを表示

--- SimpleWorkoutMemo/views/ContinuousRecordView.swift
+++ SimpleWorkoutMemo/views/ContinuousRecordView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import Lottie
 
 struct ContinuousRecordView: View {
     @EnvironmentObject var appStorageManager: AppStorageManager
@@ -14,6 +15,10 @@ struct ContinuousRecordView: View {
     @State var isShowChangeTargetDay = false
     
     let viewModel: ReportViewModel
+    
+    var disPlayWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
     
     var body: some View {
         Button {
@@ -31,37 +36,46 @@ struct ContinuousRecordView: View {
                         .frame(width: 20, height: 20)
                         .foregroundStyle(.white)
                 }
-                VStack(alignment: .center, spacing: 0) {
-                    Text("現在の記録")
-                        .foregroundStyle(.white)
-                        .font(.regular(size: 16))
-                    Text(String(viewModel.thisWeekWorkoutCount(workoutDays: workoutDays)))
-                        .foregroundStyle(.white)
-                        .font(.bold(size: 80))
-                    Text("日間")
-                        .foregroundStyle(.white)
-                        .font(.medium(size: 20))
+                ZStack {
                     if appStorageManager.settingTargetDays == viewModel.thisWeekWorkoutCount(workoutDays: workoutDays) ||
                         appStorageManager.settingTargetDays < viewModel.thisWeekWorkoutCount(workoutDays: workoutDays) {
-                        Text("目標達成しました！🔥\nおめでとうございます！😊\n来週も頑張りましょう！")
-                            .foregroundStyle(.white)
-                            .font(.regular(size: 16))
-                            .padding(.top, 20)
-                            .multilineTextAlignment(.center)
-                    } else {
-                        Text("残り\(appStorageManager.settingTargetDays - viewModel.thisWeekWorkoutCount(workoutDays: workoutDays))日トレーニングすれば\n目標達成です！🔥")
-                            .foregroundStyle(.white)
-                            .font(.regular(size: 16))
-                            .padding(.top, 20)
-                            .multilineTextAlignment(.center)
+                        LottieView(animation: .named("Confetti"))
+                            .playing()
+                            .offset(y: -40)
+                            .scaledToFit()
                     }
+                    VStack(alignment: .center, spacing: 0) {
+                        Text("現在の記録")
+                            .foregroundStyle(.white)
+                            .font(.regular(size: 16))
+                        Text(String(viewModel.thisWeekWorkoutCount(workoutDays: workoutDays)))
+                            .foregroundStyle(.white)
+                            .font(.bold(size: 80))
+                        Text("日間")
+                            .foregroundStyle(.white)
+                            .font(.medium(size: 20))
+                        if appStorageManager.settingTargetDays == viewModel.thisWeekWorkoutCount(workoutDays: workoutDays) ||
+                            appStorageManager.settingTargetDays < viewModel.thisWeekWorkoutCount(workoutDays: workoutDays) {
+                            Text("目標達成しました！🔥\nおめでとうございます！😊\n来週も頑張りましょう！")
+                                .foregroundStyle(.white)
+                                .font(.regular(size: 16))
+                                .padding(.top, 20)
+                                .multilineTextAlignment(.center)
+                        } else {
+                            Text("残り\(appStorageManager.settingTargetDays - viewModel.thisWeekWorkoutCount(workoutDays: workoutDays))日トレーニングすれば\n目標達成です！🔥")
+                                .foregroundStyle(.white)
+                                .font(.regular(size: 16))
+                                .padding(.top, 20)
+                                .multilineTextAlignment(.center)
+                        }
+                    }
+                    .padding(.vertical, 20)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        LinearGradient(colors: [.blue.opacity(0.4), .cyan.opacity(0.4)], startPoint: .top, endPoint: .bottom)
+                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
                 }
-                .padding(.vertical, 20)
-                .frame(maxWidth: .infinity)
-                .background(
-                    LinearGradient(colors: [.blue.opacity(0.4), .cyan.opacity(0.4)], startPoint: .top, endPoint: .bottom)
-                )
-                .clipShape(RoundedRectangle(cornerRadius: 12))
             }
             .padding(.horizontal, 20)
         }
